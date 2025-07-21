b1f6bc2559189dd232bc5ecb2a0bd6b63ae38e05
Ubuntu <ubuntu@ip-10-0-10-137.ap-south-1.compute.internal>
2025-07-21 07:36:04 +0000

    Two files added to Don

---
+++ add.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# write the programm to add two number
+
+echo enter the first number 
+read num 
+
+echo enter the second number 
+read num1
+
+sum=$(( num + num1))
+
+echo " The sum is" $sum
+
---
+++ file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# print number from the 1 to 5 suing for loop
+
+ for i in {1..5}
+do 
+echo" The number is :"$i
+done
